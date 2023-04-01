0e088c9fab9d42b630de65b16b8e20f84ec5e3df
Harshadkumar Patel <64983530+Harshpateloo7@users.noreply.github.com>
2023-04-01 13:34:23 -0400

    Updated Patient Model class and database

---
+++ HospitalManagementSystem/Models/Patient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HospitalManagementSystem.Models
+{
+    public class Patient
+    {
+        [Key]
+        public int PatientId { get; set; }
+
+        public string PatientName { get; set;}
+
+        public string PatientPrescription { get; set; }
+
+        public string PatientEmail { get; set; }
+
+        public string PatientPhone { get; set;}
+
+        public string PatientAddress { get; set; }
+
+
+    }
+}
