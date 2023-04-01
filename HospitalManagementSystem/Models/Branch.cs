c2d2666f22db2ae7946c907af4c302d32525184c
Harshadkumar Patel <64983530+Harshpateloo7@users.noreply.github.com>
2023-04-01 13:56:22 -0400

    Update Branch Model Class and database table

---
+++ HospitalManagementSystem/Models/Branch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HospitalManagementSystem.Models
+{
+    public class Branch
+    {
+        [Key]
+        public int BranchId { get; set; }
+
+        public string BranchName { get; set; }
+
+        public string BranchEmail { get; set; }
+
+        public string BranchPhone { get; set; }
+
+        public string BranchAddress { get; set; }
+    }
+}
