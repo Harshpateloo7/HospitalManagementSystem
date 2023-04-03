9c72077fcb0f2b8603e6d09db4c19439ee54d681
Harshadkumar Patel <64983530+Harshpateloo7@users.noreply.github.com>
2023-04-03 19:44:17 -0400

    Created BranchDataController

---
+++ HospitalManagementSystem/Controllers/BrancheDataController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using HospitalManagementSystem.Models;
+
+namespace HospitalManagementSystem.Controllers
+{
+    public class BrancheDataController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/BrancheData
+        public IQueryable<Branch> GetBranches()
+        {
+            return db.Branches;
+        }
+
+        // GET: api/BrancheData/5
+        [ResponseType(typeof(Branch))]
+        public IHttpActionResult GetBranch(int id)
+        {
+            Branch branch = db.Branches.Find(id);
+            if (branch == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(branch);
+        }
+
+        // PUT: api/BrancheData/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutBranch(int id, Branch branch)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != branch.BranchId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(branch).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BranchExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/BrancheData
+        [ResponseType(typeof(Branch))]
+        public IHttpActionResult PostBranch(Branch branch)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Branches.Add(branch);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = branch.BranchId }, branch);
+        }
+
+        // DELETE: api/BrancheData/5
+        [ResponseType(typeof(Branch))]
+        public IHttpActionResult DeleteBranch(int id)
+        {
+            Branch branch = db.Branches.Find(id);
+            if (branch == null)
+            {
+                return NotFound();
+            }
+
+            db.Branches.Remove(branch);
+            db.SaveChanges();
+
+            return Ok(branch);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool BranchExists(int id)
+        {
+            return db.Branches.Count(e => e.BranchId == id) > 0;
+        }
+    }
+}
